ce10a6e0ce4b69f8510c294ec32e4273a4b5f5b4
Branden McCarthy <bmccarthy2@ucmerced.edu>
2018-11-02 10:58:07 -0700

    Updated cachelogic.c

--- cachelogic.c
+++ cachelogic.c
@@ -70,6 +70,34 @@ void init_lru(int assoc_index, int block_index)
 }
 
 /*
+	This function updates the lru information
+
+    assoc_index - the cache unit that contains the block to be modified
+    block_number - the index of the block to be modified
+
+	lru.value ranges from 0 to assoc:
+		-lru.value = 0 -----> this block has not been used
+		-lru.value = 1 -----> this is the most recently used block
+		-lru.value = assoc -> this is the least recently used block
+
+	Note: When a block gets accessed, this function gets called on that block.
+				This function will update the blocks lru.value to 1(most recently used)
+				and increment this value for all the other blocks in the set by 1.
+*/
+void update_lru(int assoc_index, int block_index)
+{
+	//Update the accessed block to be the most recently used block
+	cache[assoc_index].block[block_index].lru.value = 1;
+	//Update the rest of the blocks in the set to reflect this change
+	for(int index = 0; index < assoc; index++) {
+		if(index != block_index) {
+			cache[assoc_index].block[index].lru.value++;
+		}
+	}
+}
+
+
+/*
 	This function chooses which block should be replaced, based on replacement policy
 
 		set - the set from the cache that we want a block to be replaced in
@@ -81,13 +109,29 @@ cacheBlock* replacementBlock(cacheSet* set) {
 	cacheBlock* block = NULL;
 
 	switch(policy) {
-		case RANDOM:
+		case RANDOM://Choose a random block in the set to replace
+			block = &(set->block[randomint(assoc)]);
 			break;
-		case LRU:
+		case LRU: //The block to replace has `lru.value` == `assoc`
+			//For each block in the set
+			for(int block_index = 0; block_index < assoc; block_index++) {
+				//Check if this block is least recently used (lru.value == assoc)
+				if(set->block[block_index].lru.value == assoc) {
+					block = &(set->block[block_index]);
+				}
+			}
 			break;
-		case LFU:
-			break;
-		default:
+		case LFU:	//The block to replace has the lowest `accessCount`
+			//Initially choose the first block as the LFU block
+			block = &(set->block[0]);
+			//For each block in the set, beginning with the second block:
+			//compare the block with the LFU block
+			for(int block_index = 1; block_index < assoc; block_index++) {
+				//If the block is less frequently used than the current *LFU block
+				if(set->block[block_index].accessCount < block->accessCount) {
+					block = &(set->block[block_index]);
+				}
+			}
 			break;
 	}
 
@@ -154,17 +198,26 @@ void accessMemory(address addr, word* data, WriteEnable we)
   */
 
 	if(we == READ) {
-		for(int i = 0; i < assoc; i++) {	//Loop through all blocks in the set
-			if(cache[index].block[i].tag == tag) { 	//READ HIT!!
-				*data = cache[index].block[i].data[offset];	//Put the value found in data
+		for(int block_index = 0; block_index < assoc; block_index++) {	//Loop through all blocks in the set
+			if(cache[index].block[block_index].tag == tag) { 	//READ HIT!!
+				*data = cache[index].block[block_index].data[offset];	//Put the value found in data
+				//Highlight the word in GREEN
+				//highlight_offset(index, block_index, offset, HIT);
+				//Update replacement policy information
+				if(policy == LRU)
+					update_lru(index, block_index);
+				else if(policy == LFU)
+					cache[index].block[block_index].accessCount++;
+				//No need to continue, so break out of for loop
 				break;
 			}
 		}
+		//highlight_offset(index, 0, offset, MISS);
 		//At this point we looked through all the blocks in the set and none of the tags
 		//match the address tag. We have a READ MISS!
+		//Handle read miss here.
 	}
 	else {	//if (we == WRITE)
-		
 	}
 
   /* This call to accessDRAM occurs when you modify any of the
